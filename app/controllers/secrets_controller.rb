9e2f4d2138158d96b9031db975d12c8c09c6a5be
Arvida Jimenez <arvida.jimenez@gmail.com>
2019-08-28 19:41:11 +0000

    Automatically backed up by Learn

--- app/controllers/secrets_controller.rb
+++ app/controllers/secrets_controller.rb
@@ -1,7 +1,6 @@
 class SecretsController < ApplicationController
   before_action :require_login
   
-  
   def show 
     
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
-  root 'sessions#login'
+  root 'sessions#new'
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   post '/logout', to: 'sessions#destroy'
   
   get '/secret', to: 'secrets#show'
   
-  resources :users
 end
